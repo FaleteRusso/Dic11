ae1eff67d09811f1ee6ebfe569a0c43b674d1f65
FaleteRusso <faleterusso89@gmail.com>
2023-12-11 19:56:45 +0100

    holii

--- src/com/corenetworks/modelo/Categoria.java
+++ src/com/corenetworks/modelo/Categoria.java
@@ -1,2 +1,64 @@
-package com.corenetworks.modelo;public class Categoria {
+package com.corenetworks.modelo;
+
+import java.util.Objects;
+
+public class Categoria {
+    private int categoryId;
+    private String categoryNom;
+    private String descripcion;
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "categoryId=" + categoryId +
+                ", categoryNom='" + categoryNom + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return categoryId == categoria.categoryId && Objects.equals(categoryNom, categoria.categoryNom) && Objects.equals(descripcion, categoria.descripcion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryId, categoryNom, descripcion);
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryNom() {
+        return categoryNom;
+    }
+
+    public void setCategoryNom(String categoryNom) {
+        this.categoryNom = categoryNom;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Categoria() {
+    }
+
+    public Categoria(int categoryId, String categoryNom, String descripcion) {
+        this.categoryId = categoryId;
+        this.categoryNom = categoryNom;
+        this.descripcion = descripcion;
+    }
 }
--- src/com/corenetworks/presentacion/ProbarCategoria.java
+++ src/com/corenetworks/presentacion/ProbarCategoria.java
@@ -1,2 +1,15 @@
-package com.corenetworks.presentacion;public class ProbarCategoria {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Categoria;
+
+import java.util.ArrayList;
+
+public class ProbarCategoria {
+    public static void main(String[] args) {
+        ArrayList<Categoria>cat = new ArrayList<>();
+        cat.add(new Categoria(12,"Droguitas","manteca"));
+        cat.add(new Categoria(16,"aviones","gps"));
+        cat.add(new Categoria(21,"Where","llegó a su destino"));
+        System.out.println(cat);
+    }
 }
